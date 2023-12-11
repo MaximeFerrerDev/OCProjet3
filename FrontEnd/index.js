bd0a6443b3ca9ce0dc3433d133611f3977a268b7
MaximeFerrerDev <maximeferrer.dev@gmail.com>
2023-12-11 17:27:59 +0100

    Splitting code into unit functions for more clarity

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,3 +1,15 @@
+/* CODE STARTING WHEN THE PAGE LOADS */
+const works = await retrieveWorks()
+const categories = await retrieveCategories()
+generateWorks(works)
+generateFilterButtons(categories)
+addFilterButtonsReactions(categories)
+const loginCheck = checkLogin()
+if (loginCheck) {
+    addEditModeHeader()
+    addLogoutButton()
+    addModalStartingButton()
+}
 
 /** 
  * RETRIEVE WORKS FROM API
@@ -44,11 +56,10 @@ function generateWorks(works) {
 }
 
 /** 
- * ADDING CATEGORIES FILTERS BUTTONS AND THEIR REACTION
- * @param { Array } categories Array of objects containing each id and name of categories
+ * GENERATING CATEGORIES FILTER BUTTONS
+ *  * @param { Array } categories Array of objects containing each id and name of categories
 **/
-function addingFilterButtons(categories) {
-    /* Generating buttons */
+function generateFilterButtons(categories) {
     for (let i = 0; i < categories.length; i++) {
         const filtersButtonContainer = document.querySelector(".filters-buttons-container")
         const newFilterButton = document.createElement("button")
@@ -56,39 +67,46 @@ function addingFilterButtons(categories) {
         newFilterButton.innerText = categories[i].name
         filtersButtonContainer.appendChild(newFilterButton)
     }
+}
 
-    /* Adding listener to each individual button */
-    for (let i = 0; i < categories.length; i++) {
-        const filterButton = document.querySelector(`.filter-button-${categories[i].id}`)
-        filterButton.addEventListener("click", function() {
-            /* Deleting every work for which categoryId isn't corresponding */
-            const worksFiltered = Array.from(works)
-            for (let j = worksFiltered.length -1 ; j >= 0 ; j--){
-                if (worksFiltered[j].categoryId != categories[i].id) {
-                    worksFiltered.splice(j,1)
+
+/** 
+ * ADDING REACTIONS TO CATEGORIES FILTER BUTTONS
+ *  * @param { Array } categories Array of objects containing each id and name of categories
+**/
+function addFilterButtonsReactions(categories) {
+        /* For each individual button */
+        for (let i = 0; i < categories.length; i++) {
+            const filterButton = document.querySelector(`.filter-button-${categories[i].id}`)
+            filterButton.addEventListener("click", function() {
+                /* Deleting every work for which categoryId isn't corresponding */
+                const worksFiltered = Array.from(works)
+                for (let j = worksFiltered.length -1 ; j >= 0 ; j--){
+                    if (worksFiltered[j].categoryId != categories[i].id) {
+                        worksFiltered.splice(j,1)
+                    }
                 }
-            }
-            /* Deleting gallery and regenerating works from the new filtered array */
+                /* Deleting gallery and regenerating works from the new filtered array */
+                document.querySelector(".gallery").innerHTML = ""
+                generateWorks(worksFiltered)
+                /* Changing CSS classes to show focus */
+                const allFiltersButtons = document.querySelectorAll(".not-focus-button")
+                allFiltersButtons.forEach((button) => button.classList.remove("focus-button") )
+                filterButton.classList.add("focus-button")
+            })
+        }
+    
+        /* For the "All categories" button */
+        const allCategoriesButton = document.querySelector(".all-categories-button")
+        allCategoriesButton.addEventListener("click", function(){
+            /* Deleting gallery temporarily and regenerating works */
             document.querySelector(".gallery").innerHTML = ""
-            generateWorks(worksFiltered)
+            generateWorks(works)
             /* Changing CSS classes to show focus */
             const allFiltersButtons = document.querySelectorAll(".not-focus-button")
             allFiltersButtons.forEach((button) => button.classList.remove("focus-button") )
-            filterButton.classList.add("focus-button")
+            allCategoriesButton.classList.add("focus-button")
         })
-    }
-
-    /* All categories button */
-    const allCategoriesButton = document.querySelector(".all-categories-button")
-    allCategoriesButton.addEventListener("click", function(){
-        /* Deleting gallery temporarily and regenerating works */
-        document.querySelector(".gallery").innerHTML = ""
-        generateWorks(works)
-        /* Changing CSS classes to show focus */
-        const allFiltersButtons = document.querySelectorAll(".not-focus-button")
-        allFiltersButtons.forEach((button) => button.classList.remove("focus-button") )
-        allCategoriesButton.classList.add("focus-button")
-    })
 }
 
 /** 
@@ -100,56 +118,180 @@ function checkLogin() {
     return Boolean(token)
 }
 
+/** 
+ * ADD "EDIT MODE" HEADER
+**/
+function addEditModeHeader() {
+    const editHeader = document.createElement("div")
+    editHeader.className = "edit-header"
+    editHeader.innerHTML = '<i class="fa-regular fa-pen-to-square"></i> <p> Mode édition </p>'
+    const header = document.querySelector("header")
+    const mainHeader = document.querySelector(".main-header")
+    header.insertBefore(editHeader,mainHeader)
+}
+
+/** 
+ * TRANSFORM LOGIN BUTTON TO LOGOUT AND ADDING ITS REACTION
+**/
+function addLogoutButton() {
+        /* Switching the login button to logout */
+        const loginLogoutButton = document.querySelector(".login-logout-button")
+        loginLogoutButton.innerHTML = '<a href="login.html">logout</a>'
+    
+        /* Removing token and userId when logging out */
+        loginLogoutButton.addEventListener("click", function(){
+            window.localStorage.removeItem("userId")
+            window.localStorage.removeItem("token")
+        })
+}
+
+/** 
+ * ADDING THE BUTTON TO START MODAL WINDOW
+**/
+function addModalStartingButton() {
+    /* Adding modal window button */
+    const modalButton = document.createElement("div")
+    modalButton.className = "modal-button"
+    modalButton.innerHTML = '<i class="fa-regular fa-pen-to-square"></i> <p> modifier </p>'
+    const main = document.querySelector("main")
+    const sectionPortfolio = document.querySelector("#portfolio")
+    main.insertBefore(modalButton,sectionPortfolio)
+
+    /* Adding listener to start modal window */
+    modalButton.addEventListener("click", galleryModal)
+}
+
 /**
- * STARTING THE ADD PICTURE MODAL
+ * MAIN FUNCTION FOR THE "GALLERY" MODAL
  **/
-const startAddPictureModal = function () {
-    /* Removing the gallery modal */
+function galleryModal() {
+    openGalleryModalWindow()
+    addCloseGalleryModalListeners()
+    addOpenAddPictureModalListener()
+    addMiniatureGallery()
+}
+
+/**
+ * OPENING THE "GALLERY" MODAL WINDOW
+ **/
+function openGalleryModalWindow() {
+   const modalGallery = document.querySelector("#modal-gallery")
+   modalGallery.style.display = null
+}
+
+/**
+ * ADDING LISTENER TO CLOSE THE "GALLERY" MODAL
+ **/
+function addCloseGalleryModalListeners() {
+    const modalCloseButton = document.querySelector(".button-close-modal-gallery")
+    modalCloseButton.addEventListener("click", function() {
+        closeGalleryModal()
+        resetMiniatureGallery()
+    })
+}
+
+/**
+ * ADDING LISTENER TO OPEN THE "ADD PICTURE" MODAL
+ **/
+function addOpenAddPictureModalListener() {
+    const AddingPictureButton = document.querySelector(".add-picture-button")
+    AddingPictureButton.addEventListener("click", addPictureModal)
+}
+
+/**
+ * ADDING MINIATURE PICTURES IN THE GALLERY MODAL AND THE DELETE BUTTONS
+ **/
+function addMiniatureGallery() {
+    /* Generating miniature pictures and the remove image buttons */
+    for (let i = 0; i < works.length; i++) {
+        const workModalImage = document.createElement("img")
+        const imageSubContainer = document.createElement("div")
+        const removeImageButton = document.createElement("div")
+        removeImageButton.className= "remove-image-button"
+        const Id = works[i].id
+        removeImageButton.innerHTML= `<i class="fa-solid fa-trash-can fa-xs"></i>` 
+        imageSubContainer.className = "image-sub-container"
+        workModalImage.src = works[i].imageUrl
+        imageSubContainer.appendChild(workModalImage)
+        imageSubContainer.appendChild(removeImageButton)
+        const modalGalleryContainer = document.querySelector(".modal-gallery-container")
+        modalGalleryContainer.appendChild(imageSubContainer)
+        /* Adding listener on each button to delete work */
+        removeImageButton.addEventListener("click", async function(event) {
+            const request = new XMLHttpRequest()
+            const token = localStorage.getItem("token")
+            request.open("DELETE", `http://localhost:5678/api/works/${Id}`, true)
+            request.setRequestHeader("Authorization", "Bearer "+ token)
+            request.send(Id)
+        })
+    }
+}
+
+/**
+ * DELTING MINIATURE PICTURES IN THE GALLERY MODAL INCLUDING BUTTONS
+ **/
+function resetMiniatureGallery() {
     const modalGalleryContainer = document.querySelector(".modal-gallery-container")
     modalGalleryContainer.innerHTML = ''
+}
+
+/**
+ * CLOSING "GALLERY" MODAL WINDOW
+ **/
+function closeGalleryModal() {
     const modalGallery = document.querySelector("#modal-gallery")
     modalGallery.style.display = "none"
-   
-    /* Adding the add picture modal */
-    const addPictureModal = document.querySelector("#modal-add-picture")
-    addPictureModal.style.display = null
+}
 
-    /* Adding listener to close the modal */
+/**
+ * MAIN FUNCTION FOR THE "ADD PICTURE" MODAL
+ **/
+function addPictureModal() {
+    closeGalleryModal()
+    resetMiniatureGallery()
+    openAddPictureModal()
+    addCloseAddPictureModalListener()
+    addBackToGalleryModalListener()
+    addPreviewListener()
+    addSubmitNewWorkListener()
+}
+
+/**
+ * OPENING THE "ADD PICTURE" MODAL WINDOW
+ **/
+function openAddPictureModal() {
+   const addPictureModal = document.querySelector("#modal-add-picture")
+   addPictureModal.style.display = null
+}
+
+/**
+ * ADDING LISTENER TO CLOSE THE "ADD PICTURE" MODAL
+ **/
+function addCloseAddPictureModalListener() {
     const modalPictureCloseButton = document.querySelector(".button-close-modal-picture")
     modalPictureCloseButton.addEventListener("click",function() {
-        addPictureModal.style.display ="none"
-        /* Resetting the picture input */
-        const pictureSubmittingContainer = document.querySelector(".picture-submitting-container")
-        pictureSubmittingContainer.innerHTML= `
-            <img src="./assets/icons/picture.svg" alt="picture">
-            <label class="download-picture-button-label">
-                <input type="file" name="file" required class="download-picture-button" hidden></input>
-                <p>+ Ajouter photo</p>
-            </label>
-            <p class="max-download-info">jpg, png : 4mo max</p>
-            `
+        closeAddPictureModal()
+        resetPictureSubmit()
     })
+}
 
-    /* Adding listener to go back to the gallery modal */
+/**
+ * ADDING LISTENER TO GO BACK TO "GALLERY" MODAL FROM "ADD PICTURE" MODAL
+ **/
+function addBackToGalleryModalListener() {
     const backToGalleryModalButton = document.querySelector(".button-back-gallery-modal")
     backToGalleryModalButton.addEventListener("click", function() {
-        addPictureModal.style.display ="none"
-        const modalGalleryContainer = document.querySelector(".modal-gallery-container")
-        modalGalleryContainer.innerHTML = ''
-        /* Resetting the picture input */
-        const pictureSubmittingContainer = document.querySelector(".picture-submitting-container")
-        pictureSubmittingContainer.innerHTML= `
-            <img src="./assets/icons/picture.svg" alt="picture">
-            <label class="download-picture-button-label">
-                <input type="file" name="file" required class="download-picture-button" hidden></input>
-                <p>+ Ajouter photo</p>
-            </label>
-            <p class="max-download-info">jpg, png : 4mo max</p>
-            `
-        startGalleryModal()
+        closeAddPictureModal()
+        resetMiniatureGallery()
+        resetPictureSubmit()
+        galleryModal()
     })
+}
 
-    /* Showing preview after uploading a picture */
+/**
+ * DISPLAYING A PREVIEW AFTER UPLOADING A PICTURE
+ **/
+function addPreviewListener() {
     const downloadPictureButton = document.querySelector(".download-picture-button")
     downloadPictureButton.addEventListener("change", function(event) {
         const pictureUpload = downloadPictureButton.files 
@@ -162,14 +304,19 @@ const startAddPictureModal = function () {
             pictureSubmittingContainer.appendChild(previewPicture)
         }
     })
+}
 
+/**
+ * ADDING LISTENER TO SUBMIT NEW WORK THROUGH THE FORM
+ **/
+function addSubmitNewWorkListener() {
     /* Retrieving data from the form */
     const addedPictureTitleInput = document.querySelector("#title")
     const addedPictureCategoryInput = document.querySelector("#category")
     const submitPictureButton = document.querySelector(".submit-picture-button")
     submitPictureButton.addEventListener("click", function(event) {
         event.preventDefault()
-        const category = 1
+        const category = 1 /* Value set for testing, to be modified */
         const picture = downloadPictureButton.files[0]
         /* Creating a formdata object */
         const formData = new FormData()
@@ -183,96 +330,28 @@ const startAddPictureModal = function () {
         request.setRequestHeader("Authorization", "Bearer "+ token)
         request.send(formData)
     })
-
 }
 
 /**
- * STARTING THE GALLERY MODAL
+ * RESETTING THE PICTURE SUBMITTING CONTAINER TO DEFAULT
+ * (deleting the preview and regenerating button)
  **/
-const startGalleryModal = function () {
-    /* Opening the modal window */
-    const modalGallery = document.querySelector("#modal-gallery")
-    modalGallery.style.display = null
-
-    /* Adding listener to close the modal */
-    const modal = document.querySelector(".modal")
-    const modalCloseButton = document.querySelector(".button-close-modal-gallery")
-    const modalGalleryContainer = document.querySelector(".modal-gallery-container")
-    modalCloseButton.addEventListener("click", function() {
-       modal.style.display ="none"
-       modalGalleryContainer.innerHTML = ''
-    })
-
-    /* Adding listener to open the second modal*/
-    const AddingPictureButton = document.querySelector(".add-picture-button")
-    AddingPictureButton.addEventListener("click", startAddPictureModal)
-
-    /* Generating miniature pictures and the remove image buttons */
-    for (let i = 0; i < works.length; i++) {
-        const workModalImage = document.createElement("img")
-        const imageSubContainer = document.createElement("div")
-        const removeImageButton = document.createElement("div")
-        removeImageButton.className= "remove-image-button"
-        const Id = works[i].id
-        removeImageButton.innerHTML= `<i class="fa-solid fa-trash-can fa-xs"></i>` 
-        imageSubContainer.className = "image-sub-container"
-        workModalImage.src = works[i].imageUrl
-        imageSubContainer.appendChild(workModalImage)
-        imageSubContainer.appendChild(removeImageButton)
-        modalGalleryContainer.appendChild(imageSubContainer)
-        /* Adding listener to delete work */
-        removeImageButton.addEventListener("click", async function(event) {
-            const request = new XMLHttpRequest()
-            const token = localStorage.getItem("token")
-            request.open("DELETE", `http://localhost:5678/api/works/${Id}`, true)
-            request.setRequestHeader("Authorization", "Bearer "+ token)
-            request.send(Id)
-        })
-    }
-
+function resetPictureSubmit() {
+    const pictureSubmittingContainer = document.querySelector(".picture-submitting-container")
+    pictureSubmittingContainer.innerHTML= `
+        <img src="./assets/icons/picture.svg" alt="picture">
+        <label class="download-picture-button-label">
+            <input type="file" name="file" required class="download-picture-button" hidden></input>
+            <p>+ Ajouter photo</p>
+        </label>
+        <p class="max-download-info">jpg, png : 4mo max</p>
+        `
 }
 
-/** 
- * STARTING THE EDIT MODE
-**/
-function startEditMode() {
-    /* Generating the edit mode header */
-    const editHeader = document.createElement("div")
-    editHeader.className = "edit-header"
-    editHeader.innerHTML = '<i class="fa-regular fa-pen-to-square"></i> <p> Mode édition </p>'
-    const header = document.querySelector("header")
-    const mainHeader = document.querySelector(".main-header")
-    header.insertBefore(editHeader,mainHeader)
-
-    /* Switching the login button to logout */
-    const loginLogoutButton = document.querySelector(".login-logout-button")
-    loginLogoutButton.innerHTML = '<a href="login.html">logout</a>'
-
-    /* Removing token and userId when logging out */
-    loginLogoutButton.addEventListener("click", function(){
-        window.localStorage.removeItem("userId")
-        window.localStorage.removeItem("token")
-    })
-
-    /* Adding modal window button */
-    const modalButton = document.createElement("div")
-    modalButton.className = "modal-button"
-    modalButton.innerHTML = '<i class="fa-regular fa-pen-to-square"></i> <p> modifier </p>'
-    const main = document.querySelector("main")
-    const sectionPortfolio = document.querySelector("#portfolio")
-    main.insertBefore(modalButton,sectionPortfolio)
-
-    /* Adding listener to start modal window */
-    modalButton.addEventListener("click", startGalleryModal)
-}
-
-
-/* CODE STARTING WHEN THE PAGE LOADS */
-const works = await retrieveWorks()
-const categories = await retrieveCategories()
-generateWorks(works)
-addingFilterButtons(categories)
-const loginCheck = checkLogin()
-if (loginCheck) {
-    startEditMode()
+/**
+ * CLOSING "ADD PICTURE" MODAL WINDOW
+ **/
+function closeAddPictureModal() {
+    const addPictureModal = document.querySelector("#modal-add-picture")
+    addPictureModal.style.display ="none"
 }
